dd6e8449a784f4967a08fa8f4131ca7fc3c2af28
CERENSULTANÇETİN <CERENSULTANETN@CEREN>
2025-01-07 03:21:01 +0300

    Proje dosyası ekle.

---
+++ GroundStation.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace RocketInterface2
+{
+    partial class GroundStation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            teststationbutton = new Button();
+            payloadbutton = new Button();
+            groundstationbutton = new Button();
+            rocketlogopictureBox = new PictureBox();
+            alıcıayarlarıgroupBox = new GroupBox();
+            connectbutton = new Button();
+            label1port = new Label();
+            label3port = new Label();
+            label2port = new Label();
+            port3radioButton = new RadioButton();
+            port2radioButton = new RadioButton();
+            port1radioButton = new RadioButton();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            port4radioButton = new RadioButton();
+            label4port = new Label();
+            verigonderbutton = new Button();
+            autoverigonderbutton = new Button();
+            vericiayarlarıgroupBox = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)rocketlogopictureBox).BeginInit();
+            alıcıayarlarıgroupBox.SuspendLayout();
+            vericiayarlarıgroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // teststationbutton
+            // 
+            teststationbutton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            teststationbutton.Location = new Point(3, 240);
+            teststationbutton.Name = "teststationbutton";
+            teststationbutton.Size = new Size(176, 59);
+            teststationbutton.TabIndex = 1;
+            teststationbutton.Text = "Test Station";
+            teststationbutton.UseVisualStyleBackColor = true;
+            teststationbutton.Click += teststationbutton_Click;
+            // 
+            // payloadbutton
+            // 
+            payloadbutton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            payloadbutton.Location = new Point(3, 305);
+            payloadbutton.Name = "payloadbutton";
+            payloadbutton.Size = new Size(180, 59);
+            payloadbutton.TabIndex = 2;
+            payloadbutton.Text = "Payload";
+            payloadbutton.UseVisualStyleBackColor = true;
+           
+            // 
+            // groundstationbutton
+            // 
+            groundstationbutton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            groundstationbutton.Location = new Point(3, 175);
+            groundstationbutton.Name = "groundstationbutton";
+            groundstationbutton.Size = new Size(176, 59);
+            groundstationbutton.TabIndex = 3;
+            groundstationbutton.Text = "Ground Station";
+            groundstationbutton.UseVisualStyleBackColor = true;
+            // 
+            // rocketlogopictureBox
+            // 
+            rocketlogopictureBox.Image = Properties.Resources.Screenshot_2025_01_05_120618;
+            rocketlogopictureBox.Location = new Point(3, 14);
+            rocketlogopictureBox.Name = "rocketlogopictureBox";
+            rocketlogopictureBox.Size = new Size(176, 157);
+            rocketlogopictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            rocketlogopictureBox.TabIndex = 4;
+            rocketlogopictureBox.TabStop = false;
+            // 
+            // alıcıayarlarıgroupBox
+            // 
+            alıcıayarlarıgroupBox.BackColor = SystemColors.ControlLight;
+            alıcıayarlarıgroupBox.Controls.Add(connectbutton);
+            alıcıayarlarıgroupBox.Controls.Add(label1port);
+            alıcıayarlarıgroupBox.Controls.Add(label3port);
+            alıcıayarlarıgroupBox.Controls.Add(label2port);
+            alıcıayarlarıgroupBox.Controls.Add(port3radioButton);
+            alıcıayarlarıgroupBox.Controls.Add(port2radioButton);
+            alıcıayarlarıgroupBox.Controls.Add(port1radioButton);
+            alıcıayarlarıgroupBox.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            alıcıayarlarıgroupBox.Location = new Point(208, 14);
+            alıcıayarlarıgroupBox.Name = "alıcıayarlarıgroupBox";
+            alıcıayarlarıgroupBox.Size = new Size(204, 138);
+            alıcıayarlarıgroupBox.TabIndex = 5;
+            alıcıayarlarıgroupBox.TabStop = false;
+            alıcıayarlarıgroupBox.Text = "Alıcı Ayarları";
+            // 
+            // connectbutton
+            // 
+            connectbutton.Location = new Point(43, 109);
+            connectbutton.Name = "connectbutton";
+            connectbutton.Size = new Size(94, 29);
+            connectbutton.TabIndex = 7;
+            connectbutton.Text = "Bağlan";
+            connectbutton.UseVisualStyleBackColor = true;
+            connectbutton.Click += connectbutton_Click;
+            // 
+            // label1port
+            // 
+            label1port.AutoSize = true;
+            label1port.BackColor = SystemColors.ControlLight;
+            label1port.ForeColor = Color.FromArgb(192, 0, 0);
+            label1port.Location = new Point(92, 28);
+            label1port.Name = "label1port";
+            label1port.Size = new Size(96, 20);
+            label1port.TabIndex = 6;
+            label1port.Text = "Bağlantı Yok";
+            // 
+            // label3port
+            // 
+            label3port.AutoSize = true;
+            label3port.ForeColor = Color.FromArgb(192, 0, 0);
+            label3port.Location = new Point(92, 83);
+            label3port.Name = "label3port";
+            label3port.Size = new Size(96, 20);
+            label3port.TabIndex = 4;
+            label3port.Text = "Bağlantı Yok";
+            // 
+            // label2port
+            // 
+            label2port.AutoSize = true;
+            label2port.ForeColor = Color.FromArgb(192, 0, 0);
+            label2port.Location = new Point(92, 55);
+            label2port.Name = "label2port";
+            label2port.Size = new Size(96, 20);
+            label2port.TabIndex = 3;
+            label2port.Text = "Bağlantı Yok";
+            // 
+            // port3radioButton
+            // 
+            port3radioButton.AutoSize = true;
+            port3radioButton.BackColor = SystemColors.ControlDark;
+            port3radioButton.Location = new Point(6, 83);
+            port3radioButton.Name = "port3radioButton";
+            port3radioButton.Size = new Size(73, 24);
+            port3radioButton.TabIndex = 2;
+            port3radioButton.TabStop = true;
+            port3radioButton.Text = "Port 3";
+            port3radioButton.UseVisualStyleBackColor = false;
+            // 
+            // port2radioButton
+            // 
+            port2radioButton.AutoSize = true;
+            port2radioButton.BackColor = SystemColors.ControlDark;
+            port2radioButton.Location = new Point(6, 53);
+            port2radioButton.Name = "port2radioButton";
+            port2radioButton.Size = new Size(73, 24);
+            port2radioButton.TabIndex = 1;
+            port2radioButton.TabStop = true;
+            port2radioButton.Text = "Port 2";
+            port2radioButton.UseVisualStyleBackColor = false;
+            // 
+            // port1radioButton
+            // 
+            port1radioButton.AutoSize = true;
+            port1radioButton.BackColor = SystemColors.ControlDark;
+            port1radioButton.Location = new Point(6, 26);
+            port1radioButton.Name = "port1radioButton";
+            port1radioButton.Size = new Size(73, 24);
+            port1radioButton.TabIndex = 0;
+            port1radioButton.TabStop = true;
+            port1radioButton.Text = "Port 1";
+            port1radioButton.UseVisualStyleBackColor = false;
+            // 
+            // port4radioButton
+            // 
+            port4radioButton.AutoSize = true;
+            port4radioButton.BackColor = SystemColors.ControlDark;
+            port4radioButton.Location = new Point(6, 34);
+            port4radioButton.Name = "port4radioButton";
+            port4radioButton.Size = new Size(73, 24);
+            port4radioButton.TabIndex = 0;
+            port4radioButton.TabStop = true;
+            port4radioButton.Text = "Port 4";
+            port4radioButton.UseVisualStyleBackColor = false;
+            // 
+            // label4port
+            // 
+            label4port.AutoSize = true;
+            label4port.ForeColor = Color.FromArgb(192, 0, 0);
+            label4port.Location = new Point(92, 36);
+            label4port.Name = "label4port";
+            label4port.Size = new Size(96, 20);
+            label4port.TabIndex = 1;
+            label4port.Text = "Bağlantı Yok";
+            // 
+            // verigonderbutton
+            // 
+            verigonderbutton.Location = new Point(0, 67);
+            verigonderbutton.Name = "verigonderbutton";
+            verigonderbutton.Size = new Size(107, 29);
+            verigonderbutton.TabIndex = 2;
+            verigonderbutton.Text = "Veri Gönder";
+            verigonderbutton.UseVisualStyleBackColor = true;
+            // 
+            // autoverigonderbutton
+            // 
+            autoverigonderbutton.Location = new Point(0, 102);
+            autoverigonderbutton.Name = "autoverigonderbutton";
+            autoverigonderbutton.Size = new Size(107, 48);
+            autoverigonderbutton.TabIndex = 7;
+            autoverigonderbutton.Text = "Otomatik Veri Gönder";
+            autoverigonderbutton.UseVisualStyleBackColor = true;
+            // 
+            // vericiayarlarıgroupBox
+            // 
+            vericiayarlarıgroupBox.BackColor = SystemColors.ControlLight;
+            vericiayarlarıgroupBox.Controls.Add(autoverigonderbutton);
+            vericiayarlarıgroupBox.Controls.Add(verigonderbutton);
+            vericiayarlarıgroupBox.Controls.Add(label4port);
+            vericiayarlarıgroupBox.Controls.Add(port4radioButton);
+            vericiayarlarıgroupBox.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            vericiayarlarıgroupBox.Location = new Point(208, 158);
+            vericiayarlarıgroupBox.Name = "vericiayarlarıgroupBox";
+            vericiayarlarıgroupBox.Size = new Size(204, 156);
+            vericiayarlarıgroupBox.TabIndex = 6;
+            vericiayarlarıgroupBox.TabStop = false;
+            vericiayarlarıgroupBox.Text = "Verici Ayarları";
+            // 
+            // GroundStation
+            // 
+            AccessibleRole = AccessibleRole.None;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1015, 555);
+            Controls.Add(vericiayarlarıgroupBox);
+            Controls.Add(alıcıayarlarıgroupBox);
+            Controls.Add(rocketlogopictureBox);
+            Controls.Add(groundstationbutton);
+            Controls.Add(payloadbutton);
+            Controls.Add(teststationbutton);
+            Name = "GroundStation";
+            SizeGripStyle = SizeGripStyle.Show;
+            Text = "GroundStation";
+            ((System.ComponentModel.ISupportInitialize)rocketlogopictureBox).EndInit();
+            alıcıayarlarıgroupBox.ResumeLayout(false);
+            alıcıayarlarıgroupBox.PerformLayout();
+            vericiayarlarıgroupBox.ResumeLayout(false);
+            vericiayarlarıgroupBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button teststationbutton;
+        private Button payloadbutton;
+        private Button groundstationbutton;
+        private PictureBox rocketlogopictureBox;
+        private GroupBox alıcıayarlarıgroupBox;
+        private RadioButton port3radioButton;
+        private RadioButton port2radioButton;
+        private RadioButton port1radioButton;
+        private Label label1port;
+        private Label label3port;
+        private Label label2port;
+        private Button connectbutton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private RadioButton port4radioButton;
+        private Label label4port;
+        private Button verigonderbutton;
+        private Button autoverigonderbutton;
+        private GroupBox vericiayarlarıgroupBox;
+    }
+}
---
+++ GroundStation.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing.Text;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using System.Threading;
+using GMap.NET.WindowsForms.Markers;
+
+
+namespace RocketInterface2
+{
+    public partial class GroundStation : Form{
+        
+        
+        private SerialPort serialPort = new SerialPort();
+        private GMapControl gmapcontrol = new GMapControl();  //map için indirilen kütüphanenin sürümünün güncel olmadığını söylüyor. versioyonu değiştirdim ancak olmadı.
+        private GMapOverlay markersOverlay;
+
+        public GroundStation(){
+            
+            InitializeComponent();
+
+            // mevcut portlar
+            var ports = SerialPort.GetPortNames();
+            port1radioButton.Enabled = ports.Contains("COM1");
+            port2radioButton.Enabled = ports.Contains("COM2");
+            port3radioButton.Enabled = ports.Contains("COM3");
+            port4radioButton.Enabled = ports.Contains("COM4");
+
+            // portların durumlarını başlatır
+            label1port.Text = "Bağlantı Yok";
+            label2port.Text = "Bağlantı Yok";
+            label3port.Text = "Bağlantı Yok";
+            label4port.Text = "Bağlantı Yok";
+
+            InitializeMap(); //haritayı başlatır
+        }
+
+
+        private void InitializeMap(){
+            // Harita kontrolünü oluştur ve ayarla
+            gmapcontrol = new GMapControl{
+                Dock = DockStyle.Fill,
+                MapProvider = GMapProviders.GoogleMap,
+                Position = new PointLatLng(39.92077, 32.85411), // Başlangıç konumu rastgele ankaranın kordinatları.
+                MinZoom = 5,
+                MaxZoom = 18,
+                Zoom = 10,
+            };
+
+            // İşaretçi katmanını oluştur
+            markersOverlay = new GMapOverlay("markers");
+            gmapcontrol.Overlays.Add(markersOverlay);
+            this.Controls.Add(gmapcontrol);
+        }
+
+        public void UpdateMarker(double latitude, double longitude){
+            markersOverlay.Markers.Clear();
+            var marker = new GMarkerGoogle(new PointLatLng(latitude, longitude), GMarkerGoogleType.red_dot);
+            markersOverlay.Markers.Add(marker);
+            gmapcontrol.Position = new PointLatLng(latitude, longitude);
+        }
+
+        private void UpdateConnectionStatus(string status){
+            Color statusColor = status == "Bağlandı" ? Color.Green : Color.Red;
+
+            // Port durumunu güncelle
+            if (port1radioButton.Checked){
+                label1port.Text = status;
+                label1port.ForeColor = statusColor;
+            }else if (port2radioButton.Checked){
+                label2port.Text = status;
+                label2port.ForeColor = statusColor;
+            }else if (port3radioButton.Checked){
+                label3port.Text = status;
+                label3port.ForeColor = statusColor;
+            }else if (port4radioButton.Checked){
+                label4port.Text = status;
+                label4port.ForeColor = statusColor;
+            }
+
+        }
+
+        private void connectbutton_Click(object sender, EventArgs e){
+            try{
+                if (serialPort.IsOpen){
+                    serialPort.Close();
+                    UpdateConnectionStatus("Bağlantı Yok");
+                }else{
+                    // Hangi port seçilmişse ona göre COM port bağla
+                    if (port1radioButton.Checked && serialPort.PortName != "COM1")
+                        serialPort.PortName = "COM1";
+                    else if (port2radioButton.Checked && serialPort.PortName != "COM2")
+                        serialPort.PortName = "COM2";
+                    else if (port3radioButton.Checked && serialPort.PortName != "COM3")
+                        serialPort.PortName = "COM3";
+                    else if (port4radioButton.Checked && serialPort.PortName != "COM4")
+                        serialPort.PortName = "COM4";
+                    else{
+                        MessageBox.Show("Lütfen bir port seçin veya geçerli bir port seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // pport ayarları
+                    serialPort.BaudRate = 9600;
+                    serialPort.DataBits = 8;
+                    serialPort.Parity = Parity.None; // verinin doğruluğunu kontrol ediyo
+                    serialPort.StopBits = StopBits.One; // stop bitini belirtir
+
+                    serialPort.Open();
+                    UpdateConnectionStatus("Bağlandı");
+                }
+            }
+
+            catch (UnauthorizedAccessException){
+                MessageBox.Show("Seçilen porta erişim sağlanamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex){
+                MessageBox.Show($"Bağlantı sırasında bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+        // formlar arası geçişteki hatanın çözümünü biliyorum. düzenleyeceğim 
+        private void teststationbutton_Click(object sender, EventArgs e){
+            TestStation testStation = new TestStation();
+            testStation.Show();
+
+            this.Hide();
+        }
+
+      
+    }
+}
